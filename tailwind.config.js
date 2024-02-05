415e6e5a38a8478053e4df943b97374da3183177
sukanyagurav <sukanyagurav6@gmail.com>
2024-02-05 22:54:22 +0530

    completed

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '1020px',
+        xl: '1440px',
+      },
+      extend: {
+        colors: {
+          bgYellow: 'hsl(47, 88%, 63%)',
+          white: 'hsl(0, 0%, 100%)',
+          grey: 'hsl(0, 0%, 50%)',
+          black: 'hsl(0, 0%, 7%)',
+        },
+    fontFamily: {
+        'sans': ['Figtree',]
+      }
+      },
+    },
+    plugins: [ ],
+  }
