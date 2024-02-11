892ce27606b3662da0b7118c2b1c247157e39a13
sukanyagurav <sukanyagurav6@gmail.com>
2024-02-11 20:51:59 +0530

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,10 @@ module.exports = {
           white: 'hsl(0, 0%, 100%)',
           grey: 'hsl(0, 0%, 50%)',
           black: 'hsl(0, 0%, 7%)',
+          
         },
     fontFamily: {
-        'sans': ['Figtree',]
+        'sans': ['Figtree','sans']
       }
       },
     },
